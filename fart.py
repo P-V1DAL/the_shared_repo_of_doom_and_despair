f806d99a6ceb18541f5601da70cdbd089822b4c8
Paolo Vidal <paolov717@gmail.com>
2025-09-18 11:11:23 -0500

    HELP

--- fart.py
+++ fart.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 
 window = tk.Tk()
 window.title('demo')
-window.geometry('300x150')
+window.geometry('1280x720')
 
 title_label = ttk.Label(master = window, text = "hello world")
 window.mainloop()
